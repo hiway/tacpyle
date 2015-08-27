7afe62d8e418291bd23585d228742a45036f8914
Harshad Sharma <harshad@sharma.io>
2015-08-27 16:23:43 +0530

    `set window size` and `set window position` methods are now available.

--- tacpyle/window.py
+++ tacpyle/window.py
@@ -1,4 +1,7 @@
+import os
 import subprocess
+from tacpyle.osahelper import osascript_multiline_compress
+
 
 def get_screen_resolution():
     """
@@ -9,3 +12,29 @@ def get_screen_resolution():
     output = output.strip().split(',')
     output = [int(x.strip()) for x in output]
     return output[2:]
+
+
+def set_window_size(window_name, width, height):
+    """
+    set_window_size("iTerm", 800, 600)
+    """
+    script = """tell application "System Events" to tell process "%s"
+    tell window 1
+        set size to {%s, %s}
+    end tell
+end tell""" % (window_name, width, height)
+    os.system(osascript_multiline_compress(script))
+
+
+def set_window_position(window_name, x, y):
+    """
+    set_window_position('iTerm', 100, 100)
+    """
+    script = """tell application "System Events" to tell process "%s"
+    tell window 1
+        set position to {%s, %s}
+    end tell
+end tell""" % (window_name, x, y)
+    os.system(osascript_multiline_compress(script))
+
+
