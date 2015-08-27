17b559fb95d3fe779ea1dd199f130ee3700bb3d2
Harshad Sharma <harshad@sharma.io>
2015-08-27 18:17:48 +0530

    Barebones MIDI listener.

---
+++ tacpyle/midi.py
@@ -0,0 +1,10 @@
+from time import sleep
+import rtmidi_python as rtmidi
+
+def start_listening(callback_method, port=0, sleep_time=0.2):
+    midi_in = rtmidi.MidiIn()
+    midi_in.callback = callback_method
+    midi_in.open_port(port)
+
+    while True:
+        sleep(sleep_time)
