a5a6dc8dc4aa24a3bc56c43155edca0d23baf459
Harshad Sharma <harshad@sharma.io>
2015-08-27 19:29:22 +0530

    One more example.

--- example.py
+++ example.py
@@ -4,4 +4,21 @@ from tacpyle.app import tacpyle_app, callback
 def fader(value):
     print value
 
+@callback(note=72, range=10)
+def say_hello(value):
+    messages = [
+        'hi',
+        'hello',
+        'well, hello there',
+        'hello again',
+        'i see you are still here',
+        'yes?',
+        'no',
+        'stop',
+        'go away',
+        'shoo!',    
+        'outta here!',
+    ]
+    print messages[value]
+
 tacpyle_app()
